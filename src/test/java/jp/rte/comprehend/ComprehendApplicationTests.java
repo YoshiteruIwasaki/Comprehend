19f9fb0d17a736e8d2ed0218c935191077088282
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2020-09-07 22:57:56 +0900

    first commit

---
+++ src/test/java/jp/rte/comprehend/ComprehendApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.rte.comprehend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprehendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
